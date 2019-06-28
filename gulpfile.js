0795c58c95081f3ba5d85c5dd2681eb2698c16c9
Chris Gradwohl <christophergradwohl@gmail.com>
2019-06-27 21:07:01 -0700

    rename dist file to APP_NAME

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,17 @@ const csso = require('gulp-csso');
 const del = require('del');
 const gulp = require('gulp');
 const postcss = require('gulp-postcss');
+const rename = require("gulp-rename");
+const package = require('./package.json');
 
 gulp.task('styles', () => {
-  return gulp.src('./public/styles/main.css')
+  return gulp.src('./main.css')
     // Auto-prefix css styles for cross browser compatibility
     .pipe(postcss([ autoprefixer() ]))
     // Minify the file
     .pipe(csso())
+    // rename swag
+    .pipe(rename(`${package.APP_NAME}.min.css`))
     // Output
     .pipe(gulp.dest('./dist'))
 });
