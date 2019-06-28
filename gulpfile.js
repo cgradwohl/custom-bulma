b58c4d0ac0c473fe1e0d05981419ac9bbca7a405
Chris Gradwohl <christophergradwohl@gmail.com>
2019-06-27 20:15:02 -0700

    adds gulp file

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const autoprefixer = require('gulp-autoprefixer');
+const csso = require('gulp-csso');
+const del = require('del');
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+
+gulp.task('styles', () => {
+  return gulp.src('./public/styles/main.css')
+    // Auto-prefix css styles for cross browser compatibility
+    .pipe(postcss([ autoprefixer() ]))
+    // Minify the file
+    .pipe(csso())
+    // Output
+    .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('clean', () => del(['dist']));
